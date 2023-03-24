a0ab31944f5776533800014deb7fdf43c13b49aa
Dave Wonnah <wonnahdave@gmail.com>
2023-03-24 16:52:24 +0000

    completed buttons and operators

---
+++ .github/calculator.js
@@ -0,0 +1,42 @@
+// const screen = document.getElementById('screen')
+// console.log(screen)
+
+// const keyboard = document.getElementsByClassName('keyboard')
+// console.log()
+
+let calculator_input_Number_V = document.getElementById('calculator_input_Number');
+let calculator_input_number_previews_V = document.getElementById('calculator_input_number_previews');
+
+let calculator_input_AC_V = document.getElementById('calculator_input_AC');
+
+let calculator_switch_theme = document.getElementsByClassName('switch');
+
+let DisplayValue;
+
+function display(DisplayValue){
+    calculator_input_Number_V.value+=DisplayValue;
+
+}
+
+calculator_input_AC_V.addEventListener('click',()=>{
+    calculator_input_Number_V.value="";
+    calculator_input_number_previews_V.value="";
+})
+
+function displaySolve(){
+    
+    let eq = calculator_input_Number_V.value;
+
+    calculator_input_number_previews_V.value=eq;
+
+    let y= eval(eq);
+
+    calculator_input_Number_V.value=y;
+}
+
+
+
+
+
+
+
